fad96f6f595900b049e3b106b8fdd33a87a13d18
Seth <sethchangcom@gmail.com>
2023-10-29 00:49:09 -0700

    Night 3 Commit lol

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 import EntryPage from '../components/EntryPage.vue';
 import ManageAccount from '../components/ManageAccount.vue';
 import PostingPage from '../components/PostingPage.vue';
-import EventBus from '../eventBus'; // Import the EventBus
-import { store } from '../main';
 
 const routes = [
   { path: '/', name: 'EntryPage', component: EntryPage },
@@ -16,35 +14,35 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = store.state.accounts.isAuthenticated;
-  console.log("isAuthenticated:", isAuthenticated);
-  console.log("Navigating to:", to.path);
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = store.state.accounts.isAuthenticated;
+//   console.log("isAuthenticated:", isAuthenticated);
+//   console.log("Navigating to:", to.path);
 
-  // If the user is authenticated and tries to visit the root path, redirect to '/posting'
-  if (isAuthenticated && to.path === '/') {
-    next('/posting');
-    return;
-  }
+//   // If the user is authenticated and tries to visit the root path, redirect to '/posting'
+//   if (isAuthenticated && to.path === '/') {
+//     next('/posting');
+//     return;
+//   }
 
-  // Check if the route requires authentication
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!isAuthenticated) {
-      next({ path: '/' });
-    } else {
-      next(); // Allow access if the user is authenticated
-    }
-  } else {
-    next(); // Allow access by default
-  }
-});
+//   // Check if the route requires authentication
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     if (!isAuthenticated) {
+//       next({ path: '/' });
+//     } else {
+//       next(); // Allow access if the user is authenticated
+//     }
+//   } else {
+//     next(); // Allow access by default
+//   }
+// });
 
-// Listen to the token-refresh-failed event from the EventBus
-EventBus.on('token-refresh-failed', () => {
-    store.commit('accounts/setAuthentication', false);
-    if (router.currentRoute.value.path !== '/') {
-        router.push('/');
-    }
-});
+// // Listen to the token-refresh-failed event from the EventBus
+// EventBus.on('token-refresh-failed', () => {
+//     store.commit('accounts/setAuthentication', false);
+//     if (router.currentRoute.value.path !== '/') {
+//         router.push('/');
+//     }
+// });
 
 export default router;
