6baae66abf1c241e4d3ec6867e341ab2a733e9e9
Seth <sethchangcom@gmail.com>
2023-11-18 18:45:35 -0800

    updated server options

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,12 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https: {
-      key: fs.readFileSync('D:\\Projects\\Python\\math-quiz\\key.pem'),
-      cert: fs.readFileSync('D:\\Projects\\Python\\math-quiz\\cert.pem'),
+    server: {
+      type: 'https',
+      options: {
+        key: fs.readFileSync('D:\\Projects\\Python\\math-quiz\\key.pem'),
+        cert: fs.readFileSync('D:\\Projects\\Python\\math-quiz\\cert.pem'),
+      }
     }
   }
 });
